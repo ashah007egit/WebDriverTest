fb5c4ddf35896c14f1b20332590c9b20e8f5f45a
Ankit Shah <ashah007@gmail.com>
2019-12-01 20:57:53 -0600

    Initial Maven Project

---
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,28 @@
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver1;		
+		@Test				
+		public void testEasy() {	
+			driver1.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver1.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+			System.out.println(title);
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+		    driver1 = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver1.quit();			
+		}		
+}	
